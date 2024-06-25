62f56a88ee0935d3cd56a3e2f5f1e89f9ff1b6f7
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-06-25 21:52:53 +0200

    Fixing .github folder and adding a ping command and avatar

--- src/main/kotlin/com.jotrorox.jonas/Main.kt
+++ src/main/kotlin/com.jotrorox.jonas/Main.kt
@@ -1,5 +1,7 @@
 package com.jotrorox.jonas
 
+import dev.kord.cache.api.data.description
+import dev.kord.common.Color
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
 import dev.kord.core.behavior.interaction.response.respond
@@ -7,6 +9,7 @@ import dev.kord.core.event.interaction.GuildChatInputCommandInteractionCreateEve
 import dev.kord.core.on
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
+import dev.kord.rest.builder.message.embed
 
 suspend fun main() {
     val token =  System.getenv("DISCORD_TOKEN") ?: error("No token provided.")
@@ -24,9 +27,25 @@ suspend fun main() {
 
         if (command.rootName.equals("ping")) {
             val response = interaction.deferEphemeralResponse()
+            val ping = interaction.kord.gateway.averagePing
+
+            var embed_color = Color(0, 0, 255)
+
+            if (ping?.inWholeMilliseconds!! > 125) embed_color = Color(0, 255, 0)
+            if (ping.inWholeMilliseconds > 250) embed_color = Color(255, 255, 0)
+            if (ping.inWholeMilliseconds > 500) embed_color = Color(255, 0, 0)
+
 
             response.respond {
-                content = "This hasn't been implemented yet"
+                embed {
+                    color = embed_color
+                    title = "Ping"
+                    description = "The current ping is: " + ping.inWholeMilliseconds + "ms"
+                    footer {
+                        text = "Jonas - made by jotrorox"
+                        icon = 
+                    }
+                }
             }
         }
     }
