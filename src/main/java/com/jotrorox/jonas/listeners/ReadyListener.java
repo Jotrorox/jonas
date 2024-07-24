79f89f7672faa3e41c8a8a5dc0e67d7c0f50a368
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-07-24 12:43:10 +0200

    Adding the presence and cleanup in the ReadyListener

--- src/main/java/com/jotrorox/jonas/listeners/ReadyListener.java
+++ src/main/java/com/jotrorox/jonas/listeners/ReadyListener.java
@@ -1,20 +1,20 @@
 package com.jotrorox.jonas.listeners;
 
 import ch.qos.logback.classic.Logger;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import org.jetbrains.annotations.NotNull;
 
 public class ReadyListener implements EventListener {
-
-
-
     @Override
     public void onEvent(@NotNull GenericEvent genericEvent) {
         if (!(genericEvent instanceof ReadyEvent)) return;
 
         Logger logger = ListenerLogger.getLogger();
-        logger.info("Bot is ready and logged in as {}", ((ReadyEvent) genericEvent).getJDA().getSelfUser().getAsTag());
+        logger.info("Bot is ready and logged in as {}", genericEvent.getJDA().getSelfUser().getAsTag());
+
+        genericEvent.getJDA().getPresence().setActivity(Activity.customStatus("This bot is still in development! Please be patient!"));
     }
 }
