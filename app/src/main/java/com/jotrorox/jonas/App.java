68590e9e0e36059c14d03717fd7404af03d3fbdb
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-10-09 22:03:50 +0200

    Add Discord JDA integration and logging configuration

--- app/src/main/java/com/jotrorox/jonas/App.java
+++ app/src/main/java/com/jotrorox/jonas/App.java
@@ -1,7 +1,16 @@
 package com.jotrorox.jonas;
 
+import java.util.Collections;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String token = System.getenv("DISCORD_TOKEN");
+
+        JDABuilder builder = JDABuilder.createLight(token, Collections.emptyList());
+
+        JDA jda = builder.build();
     }
 }
